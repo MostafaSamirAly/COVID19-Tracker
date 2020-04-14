5640b0a46be27a6541a5c81a0ce2099e5fc2e0d0
Mostafa <samir.mostafa868@gmail.com>
2020-04-14 19:31:25 +0200

    check connectivity added

--- app/src/main/java/com/example/covid19_tracker/MainActivity.kt
+++ app/src/main/java/com/example/covid19_tracker/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.covid19_tracker
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -29,18 +32,43 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        viewModel.getNewData().observe(this, Observer<List<Country>> { countries ->
-            // update UI
-//            if(countries != null){
-//                for (country in countries) {
-//                    println(country.country_name)
-//                }
-//            }else{
-//                println("Error Fetching Data")
-//            }
+        if(checkConnectivity()){
+            viewModel.getNewData().observe(this, Observer<List<Country>> { countries ->
+                // update UI
+                if(countries != null){
+                    for (country in countries) {
+                        println(country.country_name)
+                    }
+                }else{
+                    println("Error Fetching Data")
+                }
 
-        })
+            })
+        }else{
+            viewModel.getSavedData().observe(this, Observer<List<Country>> { countries ->
+                // update UI
+                if(countries != null){
+                    for (country in countries) {
+                        println(country.country_name)
+                    }
+                }else{
+                    println("Error Fetching Data")
+                }
+
+            })
+        }
+
     }
+    private fun checkConnectivity() : Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetworkInfo
+        val isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting
+        if (!isConnected) {
+            Toast.makeText(this, "Check network connection", Toast.LENGTH_SHORT).show()
+        }
+        return isConnected
+    }
+
     /*Mostafa End*/
 
     /*AboElnaga Start*/
